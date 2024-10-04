2a39370d4f06ce53db95dc2f6454eb431b239727
verafes <verafes@gmail.com>
2024-10-03 18:59:37 -0700

    user setup

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < ApplicationController
+  def destroy
+    super do |resource|
+      if resource.nil?
+        flash[:alert] = 'Logout failed. Please try again.'
+        redirect_to root_path and return
+      end
+    end
+    flash[:notice] = 'You have been logged out successfully.'
+    redirect_to root_path
+  end
+end
