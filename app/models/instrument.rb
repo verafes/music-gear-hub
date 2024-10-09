850f2142aa20f1d0bc180ea7f6bbfaf54073b453
verafes <verafes@gmail.com>
2024-10-08 22:43:44 -0700

    Refactor instrument controller and views, update image handling

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,4 +1,11 @@
 class Instrument < ApplicationRecord
-  CONDITION = ['New', 'Used', 'Refurbished']
+  belongs_to :user, optional: true
+  CONDITION = %w{ New Excellent Mint Used Fair Poor }
   has_one_attached :image
+
+  validates :name, :category, :price_per_day, :available, presence: true
+  validates :description, length: { maximum: 250, too_long: "%{count} characters is the maximum aloud. "}
+  validates :name, length: { maximum: 140, too_long: "%{count} characters is the maximum aloud. "}
+  validates :price_per_day, length: { maximum: 7 }
+
 end
